ec0f769b6a4847e92b536d7ccd40426b515a6f19
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-02-14 12:13:34 -0500

    feat: search parent directories when resolving formatter

--- src/resolveFormatter.ts
+++ src/resolveFormatter.ts
@@ -1,44 +1,71 @@
 import * as fs from "node:fs/promises";
-import { readPackageUp } from "read-package-up";
 
 import { Formatter, formatters } from "./formatters.js";
+import path from "node:path";
 
 export async function resolveFormatter(
 	cwd = ".",
 ): Promise<Formatter | undefined> {
-	const children = await fs.readdir(cwd);
+	return escalade(cwd, async (dir, files) => {
+		let getPackageData:
+			| (() => Promise<Record<string, unknown> | undefined>)
+			| undefined;
 
-	for (const child of children) {
-		for (const formatter of formatters) {
-			if (formatter.testers.configFile.test(child)) {
-				return formatter;
+		for (const file of files) {
+			if (file === "package.json") {
+				getPackageData ??= () =>
+					fs
+						.readFile(path.join(dir, file), "utf8")
+						.then(JSON.parse)
+						.catch(() => undefined);
+			} else {
+				for (const formatter of formatters) {
+					if (formatter.testers.configFile.test(file)) {
+						return formatter;
+					}
+				}
 			}
 		}
-	}
 
-	const packageData = await readPackageUp({ cwd });
-	if (!packageData) {
-		return undefined;
-	}
+		const packageData = await getPackageData?.();
+		if (packageData) {
+			const { scripts = {}, ...otherKeys } = packageData as {
+				scripts?: Record<string, string>;
+				[key: string]: unknown;
+			};
 
-	const { scripts = {}, ...otherKeys } = packageData.packageJson;
+			for (const script of Object.values(scripts)) {
+				for (const formatter of formatters) {
+					if (formatter.testers.script.test(script)) {
+						return formatter;
+					}
+				}
+			}
 
-	for (const script of Object.values(scripts)) {
-		for (const formatter of formatters) {
-			if (formatter.testers.script.test(script)) {
-				return formatter;
+			for (const formatter of formatters) {
+				if (
+					"packageKey" in formatter.testers &&
+					formatter.testers.packageKey in otherKeys
+				) {
+					return formatter;
+				}
 			}
 		}
-	}
+	});
+}
 
-	for (const formatter of formatters) {
-		if (
-			"packageKey" in formatter.testers &&
-			formatter.testers.packageKey in otherKeys
-		) {
-			return formatter;
+type Promisable<T> = T | Promise<T>;
+
+async function escalade<T extends {}>(
+	cwd: string,
+	callback: (dir: string, files: string[]) => Promisable<T | undefined | void>,
+): Promise<T | undefined> {
+	const { root } = path.parse(cwd);
+	for (let dir = cwd; dir !== root; dir = path.dirname(dir)) {
+		const files = await fs.readdir(dir);
+		const result = await callback(dir, files);
+		if (result !== undefined) {
+			return result;
 		}
 	}
-
-	return undefined;
 }
